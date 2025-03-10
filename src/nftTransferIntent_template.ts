c2a4d8987c460e05258fdfa6311a713f23c78b91
Saakshi Raut <saakshiraut28@gmail.com>
2025-03-10 17:46:59 +0530

    fix: the intent data for testing

--- src/nftTransferIntent_template.ts
+++ src/nftTransferIntent_template.ts
@@ -1,256 +1,25 @@
 import {
-  concat,
   encodeAbiParameters,
   encodeFunctionData,
-  encodePacked,
-  fromHex,
-  hexToBigInt,
-  keccak256,
-  pad,
   parseAbiParameters,
   toHex,
   type Hash,
   type Hex
 } from "viem";
-import { parse as uuidParse, v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-import { signMessage } from "viem/accounts";
-
+import { INTENT_ABI } from "./utils/abi.js";
+import { Constants } from "./utils/constants.js";
+import { paymasterData } from "./utils/paymaster.js";
+import { nonceToBigInt } from "./utils/nonceToBigInt.js";
+import { signUserOp, executeUserOp, type SessionConfig } from "./utils/userOpExecutor.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const clientPrivateKey = process.env.OKTO_CLIENT_PRIVATE_KEY as Hash;
 const clientSWA = process.env.OKTO_CLIENT_SWA as Hex;
 const OktoAuthToken = process.env.OKTO_AUTH_TOKEN as string;
-
-var INTENT_ABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_jobId",
-        type: "uint256"
-      },
-      {
-        name: "_clientSWA",
-        type: "address"
-      },
-      {
-        name: "_jobCreatorId",
-        type: "address"
-      },
-      {
-        name: "_policyInfo",
-        type: "bytes"
-      },
-      {
-        name: "_gsnData",
-        type: "bytes"
-      },
-      {
-        name: "_jobParameters",
-        type: "bytes"
-      },
-      {
-        name: "_intentType",
-        type: "string"
-      }
-    ],
-    name: "initiateJob",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  }
-];
-
-var Constants = {
-  HOURS_IN_MS: 60 * 60 * 1e3,
-  EXECUTE_USEROP_FUNCTION_SELECTOR: "0x8dd7712f",
-  FUNCTION_NAME: "initiateJob",
-  USEROP_VALUE: BigInt(0),
-  GAS_LIMITS: {
-    CALL_GAS_LIMIT: BigInt(3e5),
-    VERIFICATION_GAS_LIMIT: BigInt(2e5),
-    PRE_VERIFICATION_GAS: BigInt(5e4),
-    MAX_FEE_PER_GAS: BigInt(2e9),
-    MAX_PRIORITY_FEE_PER_GAS: BigInt(2e9),
-    PAYMASTER_POST_OP_GAS_LIMIT: BigInt(1e5),
-    PAYMASTER_VERIFICATION_GAS_LIMIT: BigInt(1e5)
-  },
-  INTENT_TYPE: {
-    TOKEN_TRANSFER: "TOKEN_TRANSFER",
-    NFT_TRANSFER: "NFT_TRANSFER",
-    NFT_COLLECTION_CREATION: "NFT_COLLECTION_CREATION",
-    RAW_TRANSACTION: "RAW_TRANSACTION"
-  },
-  ENV_CONFIG: {
-    STAGING: {
-      PAYMASTER_ADDRESS: "0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0",
-      JOB_MANAGER_ADDRESS: "0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E",
-      ENTRYPOINT_CONTRACT_ADDRESS: "0x8D29ECb381CA4874767Ef3744F6df37748B12715",
-      CHAIN_ID: 24879
-    },
-    SANDBOX: {
-      PAYMASTER_ADDRESS: "0x5408fAa7F005c46B85d82060c532b820F534437c",
-      JOB_MANAGER_ADDRESS: "0x21E822446C32FA22b29392F29597ebdcFd8511f8",
-      ENTRYPOINT_CONTRACT_ADDRESS: "0xA5E95a08229A816c9f3902E4a5a618C3928ad3bA",
-      CHAIN_ID: 8801
-    }
-    // PRODUCTION: {
-    //   PAYMASTER_ADDRESS: '0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0' as Hex,
-    //   JOB_MANAGER_ADDRESS: '0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E' as Hex,
-    //   CHAIN_ID: 24879,
-    // },
-  }
-};
-
-// this function is used to create paymaster information
-async function generatePaymasterData(
-  address: any,
-  privateKey: any,
-  nonce: any,
-  validUntil: any,
-  validAfter: any
-) {
-  if (validUntil instanceof Date) {
-    validUntil = Math.floor(validUntil.getTime() / 1e3);
-  } else if (typeof validUntil === "bigint") {
-    validUntil = parseInt(validUntil.toString());
-  }
-  if (validAfter instanceof Date) {
-    validAfter = Math.floor(validAfter.getTime() / 1e3);
-  } else if (typeof validAfter === "bigint") {
-    validAfter = parseInt(validAfter.toString());
-  } else if (validAfter === void 0) {
-    validAfter = 0;
-  }
-  const paymasterDataHash = keccak256(
-    encodePacked(
-      ["bytes32", "address", "uint48", "uint48"],
-      [
-        toHex(nonceToBigInt(nonce), { size: 32 }),
-        address,
-        validUntil,
-        validAfter,
-      ]
-    )
-  );
-  const sig = await signMessage({
-    message: {
-      raw: fromHex(paymasterDataHash, "bytes"),
-    },
-    privateKey,
-  });
-  const paymasterData = encodeAbiParameters(
-    parseAbiParameters("address, uint48, uint48, bytes"),
-    [address, validUntil, validAfter, sig]
-  );
-  return paymasterData;
-}
-
-async function paymasterData({
-  nonce,
-  validUntil,
-  validAfter
-}: any) {
-  return generatePaymasterData(
-    clientSWA,
-    clientPrivateKey,
-    nonce,
-    validUntil,
-    validAfter
-  );
-}
-
-function nonceToBigInt(nonce: string): bigint {
-  const uuidBytes = uuidParse(nonce); // Get the 16-byte array of the UUID
-  let bigInt = BigInt(0);
-  for (let i = 0; i < uuidBytes.length; i++) {
-    if (uuidBytes[i] !== undefined) {
-      bigInt = (bigInt << BigInt(8)) | BigInt(uuidBytes[i]!);
-    }
-  }
-
-  return bigInt;
-}
-
-function generatePackedUserOp(userOp: any) {
-  if (!userOp.sender || !userOp.nonce || !userOp.callData || !userOp.preVerificationGas || !userOp.verificationGasLimit || !userOp.callGasLimit || !userOp.maxFeePerGas || !userOp.maxPriorityFeePerGas || userOp.paymaster == void 0 || !userOp.paymasterVerificationGasLimit || !userOp.paymasterPostOpGasLimit || userOp.paymasterData == void 0) {
-    throw new Error("Invalid UserOp");
-  }
-  const accountGasLimits = "0x" + pad(userOp.verificationGasLimit, {
-    size: 16
-  }).toString().replace("0x", "") + pad(userOp.callGasLimit, {
-    size: 16
-  }).toString().replace("0x", "");
-  const gasFees = "0x" + pad(userOp.maxFeePerGas, {
-    size: 16
-  }).toString().replace("0x", "") + pad(userOp.maxPriorityFeePerGas, {
-    size: 16
-  }).toString().replace("0x", "");
-  const paymasterAndData = userOp.paymaster ? concat([
-    userOp.paymaster,
-    pad(userOp.paymasterVerificationGasLimit, {
-      size: 16
-    }),
-    pad(userOp.paymasterPostOpGasLimit, {
-      size: 16
-    }),
-    userOp.paymasterData
-  ]) : "0x";
-  const packedUserOp = {
-    sender: userOp.sender,
-    nonce: userOp.nonce,
-    initCode: "0x",
-    callData: userOp.callData,
-    preVerificationGas: userOp.preVerificationGas,
-    accountGasLimits,
-    gasFees,
-    paymasterAndData
-  };
-  return packedUserOp;
-}
-
-function generateUserOpHash(userOp: any) {
-  const pack = encodeAbiParameters(
-    parseAbiParameters(
-      "address, bytes32, bytes32, bytes32, bytes32, uint256, bytes32, bytes32"
-    ),
-    [
-      userOp.sender,
-      pad(userOp.nonce, {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.initCode), {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.callData), {
-        size: 32
-      }) as `0x${string}`,
-      pad(userOp.accountGasLimits, {
-        size: 32
-      }) as `0x${string}`,
-      hexToBigInt(userOp.preVerificationGas),
-      pad(userOp.gasFees, {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.paymasterAndData), {
-        size: 32
-      })
-    ]
-  );
-  const userOpPack = encodeAbiParameters(
-    parseAbiParameters("bytes32, address, uint256"),
-    [keccak256(pack), Constants.ENV_CONFIG.SANDBOX.ENTRYPOINT_CONTRACT_ADDRESS as Hex, BigInt(Constants.ENV_CONFIG.SANDBOX.CHAIN_ID)]
-  );
-  return keccak256(userOpPack);
-}
-
-interface Config {
-  clientSWA: string;
-  userSWA: string;
-}
 
 interface Data {
   caipId: string;
@@ -261,69 +30,16 @@ interface Data {
   nftType: 'ERC721' | 'ERC1155' | string;
 }
 
-interface sessionConfig {
-  sessionPrivKey: string;
-  sessionPubkey: string;
-  userSWA: string;
-}
-
-function serializeJSON(obj: any, space: any) {
-  return JSON.stringify(
-    obj,
-    (key, value) =>
-      typeof value === "bigint" ? value.toString() + "n" : value,
-    space
-  );
-}
-
-async function signUserOp(userop: any, sessionConfig: sessionConfig) {
-  const privateKey = sessionConfig.sessionPrivKey as `0x${string}`;
-  const packeduserop = generatePackedUserOp(userop);
-  const hash = generateUserOpHash(packeduserop);
-  const sig = await signMessage({
-    message: {
-      raw: fromHex(hash, "bytes")
-    },
-    privateKey
-  });
-  userop.signature = sig;
-  return userop;
-}
-
-async function executeUserOp(userop: any) {
-  try {
-    const requestBody = {
-      method: "execute",
-      jsonrpc: "2.0",
-      id: uuidv4(),
-      params: [userop],
-    };
-
-    const serliazedPayload = serializeJSON(requestBody, null);
-    const response = await axios.post(
-      "https://sandbox-okto-gateway.oktostage.com/rpc",
-      serliazedPayload,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${OktoAuthToken}`
-        },
-      }
-    );
-
-    return response.data.result.jobId;
-  } catch (error) {
-    console.error("Error executing user operation:", error);
-    throw error;
-  }
-}
-
-async function transferNft(data: Data, sessionConfig: sessionConfig) {
+/**
+ * NFT Transfer Intent: this function executes the nft transfer between addresses.
+ * For more information, check https://docs.okto.tech/docs/openapi/nftTransfer
+ */
+async function transferNft(data: Data, sessionConfig: SessionConfig) {
   const nonce = uuidv4();
   const jobParametersAbiType = "(string caip2Id, string nftId, string recipientWalletAddress, string collectionAddress, string nftType, uint amount)";
   const gsnDataAbiType = `(bool isRequired, string[] requiredNetworks, ${jobParametersAbiType}[] tokens)`;
 
-  //get supported chains on okto
+  // Fetch all enabled networks
   const getChainsResponse = await axios.get("https://sandbox-api.okto.tech/api/oc/v1/supported/networks", {
     headers: {
       "Authorization": `Bearer ${OktoAuthToken}`
@@ -497,22 +213,23 @@ async function transferNft(data: Data, sessionConfig: sessionConfig) {
   //   signature: '0x42286d3a9c6161859439509024660b8ab3b8884cecc9338d33b0d29f92d377311c2ed3a61cde3fcc04d5ad230e4a0abf16bce72d3fd0d4e832a96d1ddc031c661b'
   // }
 
-  const jobId = await executeUserOp(signedUserOp);
+  const jobId = await executeUserOp(signedUserOp, OktoAuthToken);
   console.log("JobId: ", jobId);
   // Sample Response:
   // JobId: 14778af1-9d12-42ca-b664-1686f38f3633
 }
 
+// To get the capId, please check: https://docsv2.okto.tech/docs/openapi/technical-reference
 const data: Data = {
-  caipId: 'eip155:80002',
+  caipId: 'eip155:80002', // POLYGON_AMOY_TESTNET
   collectionAddress: '0x0b5ca10156a184201d93609fd7d1606c10d5da21',
   nftId: '0',
   recipientWalletAddress: '0x597F2Fd9E42b58dF047849Bb29E25377745fF0da',
-  amount: 1,
+  amount: 1, // Should always be >0
   nftType: 'ERC721'
 }
 
-const sessionConfig: sessionConfig = {
+const sessionConfig: SessionConfig = {
   sessionPrivKey: '0x9326598622a612694dc9a251c6f246fe22a43256ecb8633ef9aadfbf21926b65',
   sessionPubkey: '0x0491d65487ac1f4b734931f1e3df02c6f137b0064a6875e6e2b537d42c823576e9ccc1c91c49eff0c33f3e490e661ecb8a39bf1398d51200870f425cad0ef70fb6',
   userSWA: '0x61795557B50DC229199cE51c46935d7eC560c52F'
--- src/rawTransactionIntent_template.ts
+++ src/rawTransactionIntent_template.ts
@@ -1,263 +1,32 @@
 import {
-  concat,
   encodeAbiParameters,
   encodeFunctionData,
-  encodePacked,
-  fromHex,
-  hexToBigInt,
-  keccak256,
-  pad,
   parseAbiParameters,
   toHex,
   stringToBytes,
   type Hash,
   type Hex
 } from "viem";
-import { parse as uuidParse, v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-import { signMessage } from "viem/accounts";
-
+import { INTENT_ABI } from "./utils/abi.js";
+import { Constants } from "./utils/constants.js";
+import { paymasterData } from "./utils/paymaster.js";
+import { nonceToBigInt } from "./utils/nonceToBigInt.js";
+import { signUserOp, executeUserOp, type SessionConfig } from "./utils/userOpExecutor.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const clientPrivateKey = process.env.OKTO_CLIENT_PRIVATE_KEY as Hash;
 const clientSWA = process.env.OKTO_CLIENT_SWA as Hex;
 const OktoAuthToken = process.env.OKTO_AUTH_TOKEN as string;
 
-var INTENT_ABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_jobId",
-        type: "uint256"
-      },
-      {
-        name: "_clientSWA",
-        type: "address"
-      },
-      {
-        name: "_jobCreatorId",
-        type: "address"
-      },
-      {
-        name: "_policyInfo",
-        type: "bytes"
-      },
-      {
-        name: "_gsnData",
-        type: "bytes"
-      },
-      {
-        name: "_jobParameters",
-        type: "bytes"
-      },
-      {
-        name: "_intentType",
-        type: "string"
-      }
-    ],
-    name: "initiateJob",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  }
-];
-
-var Constants = {
-  HOURS_IN_MS: 60 * 60 * 1e3,
-  EXECUTE_USEROP_FUNCTION_SELECTOR: "0x8dd7712f",
-  FUNCTION_NAME: "initiateJob",
-  USEROP_VALUE: BigInt(0),
-  GAS_LIMITS: {
-    CALL_GAS_LIMIT: BigInt(3e5),
-    VERIFICATION_GAS_LIMIT: BigInt(2e5),
-    PRE_VERIFICATION_GAS: BigInt(5e4),
-    MAX_FEE_PER_GAS: BigInt(2e9),
-    MAX_PRIORITY_FEE_PER_GAS: BigInt(2e9),
-    PAYMASTER_POST_OP_GAS_LIMIT: BigInt(1e5),
-    PAYMASTER_VERIFICATION_GAS_LIMIT: BigInt(1e5)
-  },
-  INTENT_TYPE: {
-    TOKEN_TRANSFER: "TOKEN_TRANSFER",
-    NFT_TRANSFER: "NFT_TRANSFER",
-    NFT_COLLECTION_CREATION: "NFT_COLLECTION_CREATION",
-    RAW_TRANSACTION: "RAW_TRANSACTION"
-  },
-  ENV_CONFIG: {
-    STAGING: {
-      PAYMASTER_ADDRESS: "0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0",
-      JOB_MANAGER_ADDRESS: "0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E",
-      ENTRYPOINT_CONTRACT_ADDRESS: "0x8D29ECb381CA4874767Ef3744F6df37748B12715",
-      CHAIN_ID: 24879
-    },
-    SANDBOX: {
-      PAYMASTER_ADDRESS: "0x5408fAa7F005c46B85d82060c532b820F534437c",
-      JOB_MANAGER_ADDRESS: "0x21E822446C32FA22b29392F29597ebdcFd8511f8",
-      ENTRYPOINT_CONTRACT_ADDRESS: "0xA5E95a08229A816c9f3902E4a5a618C3928ad3bA",
-      CHAIN_ID: 8801
-    }
-    // PRODUCTION: {
-    //   PAYMASTER_ADDRESS: '0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0' as Hex,
-    //   JOB_MANAGER_ADDRESS: '0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E' as Hex,
-    //   CHAIN_ID: 24879,
-    // },
-  }
-};
-
-// this function is used to create paymaster information
-async function generatePaymasterData(
-  address: any,
-  privateKey: any,
-  nonce: any,
-  validUntil: any,
-  validAfter: any
-) {
-  if (validUntil instanceof Date) {
-    validUntil = Math.floor(validUntil.getTime() / 1e3);
-  } else if (typeof validUntil === "bigint") {
-    validUntil = parseInt(validUntil.toString());
-  }
-  if (validAfter instanceof Date) {
-    validAfter = Math.floor(validAfter.getTime() / 1e3);
-  } else if (typeof validAfter === "bigint") {
-    validAfter = parseInt(validAfter.toString());
-  } else if (validAfter === void 0) {
-    validAfter = 0;
-  }
-  const paymasterDataHash = keccak256(
-    encodePacked(
-      ["bytes32", "address", "uint48", "uint48"],
-      [
-        toHex(nonceToBigInt(nonce), { size: 32 }),
-        address,
-        validUntil,
-        validAfter,
-      ]
-    )
-  );
-  const sig = await signMessage({
-    message: {
-      raw: fromHex(paymasterDataHash, "bytes"),
-    },
-    privateKey,
-  });
-  const paymasterData = encodeAbiParameters(
-    parseAbiParameters("address, uint48, uint48, bytes"),
-    [address, validUntil, validAfter, sig]
-  );
-  return paymasterData;
-}
-
-async function paymasterData({
-  nonce,
-  validUntil,
-  validAfter
-}: any) {
-  return generatePaymasterData(
-    clientSWA,
-    clientPrivateKey,
-    nonce,
-    validUntil,
-    validAfter
-  );
-}
-
-function nonceToBigInt(nonce: string): bigint {
-  const uuidBytes = uuidParse(nonce); // Get the 16-byte array of the UUID
-  let bigInt = BigInt(0);
-  for (let i = 0; i < uuidBytes.length; i++) {
-    if (uuidBytes[i] !== undefined) {
-      bigInt = (bigInt << BigInt(8)) | BigInt(uuidBytes[i]!);
-    }
-  }
-
-  return bigInt;
-}
-
-function generatePackedUserOp(userOp: any) {
-  if (!userOp.sender || !userOp.nonce || !userOp.callData || !userOp.preVerificationGas || !userOp.verificationGasLimit || !userOp.callGasLimit || !userOp.maxFeePerGas || !userOp.maxPriorityFeePerGas || userOp.paymaster == void 0 || !userOp.paymasterVerificationGasLimit || !userOp.paymasterPostOpGasLimit || userOp.paymasterData == void 0) {
-    throw new Error("Invalid UserOp");
-  }
-  const accountGasLimits = "0x" + pad(userOp.verificationGasLimit, {
-    size: 16
-  }).toString().replace("0x", "") + pad(userOp.callGasLimit, {
-    size: 16
-  }).toString().replace("0x", "");
-  const gasFees = "0x" + pad(userOp.maxFeePerGas, {
-    size: 16
-  }).toString().replace("0x", "") + pad(userOp.maxPriorityFeePerGas, {
-    size: 16
-  }).toString().replace("0x", "");
-  const paymasterAndData = userOp.paymaster ? concat([
-    userOp.paymaster,
-    pad(userOp.paymasterVerificationGasLimit, {
-      size: 16
-    }),
-    pad(userOp.paymasterPostOpGasLimit, {
-      size: 16
-    }),
-    userOp.paymasterData
-  ]) : "0x";
-  const packedUserOp = {
-    sender: userOp.sender,
-    nonce: userOp.nonce,
-    initCode: "0x",
-    callData: userOp.callData,
-    preVerificationGas: userOp.preVerificationGas,
-    accountGasLimits,
-    gasFees,
-    paymasterAndData
-  };
-  return packedUserOp;
-}
-
-function generateUserOpHash(userOp: any) {
-  const pack = encodeAbiParameters(
-    parseAbiParameters(
-      "address, bytes32, bytes32, bytes32, bytes32, uint256, bytes32, bytes32"
-    ),
-    [
-      userOp.sender,
-      pad(userOp.nonce, {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.initCode), {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.callData), {
-        size: 32
-      }) as `0x${string}`,
-      pad(userOp.accountGasLimits, {
-        size: 32
-      }) as `0x${string}`,
-      hexToBigInt(userOp.preVerificationGas),
-      pad(userOp.gasFees, {
-        size: 32
-      }) as `0x${string}`,
-      pad(keccak256(userOp.paymasterAndData), {
-        size: 32
-      })
-    ]
-  );
-  const userOpPack = encodeAbiParameters(
-    parseAbiParameters("bytes32, address, uint256"),
-    [keccak256(pack), Constants.ENV_CONFIG.SANDBOX.ENTRYPOINT_CONTRACT_ADDRESS as Hex, BigInt(Constants.ENV_CONFIG.SANDBOX.CHAIN_ID)]
-  );
-  return keccak256(userOpPack);
-}
-
-interface Config {
-  clientSWA: string;
-  userSWA: string;
-}
-
 interface EVMRawTransaction {
-  from: string; // Address as a string
+  from: string; 
   to: string;
-  data?: string; // Optional transaction data
-  value?: number | bigint; // Optional value as number or bigint
+  data?: string;
+  value?: number | bigint;
 }
 
 interface Data {
@@ -265,65 +34,11 @@ interface Data {
   transaction: EVMRawTransaction;
 }
 
-
-interface sessionConfig {
-  sessionPrivKey: string;
-  sessionPubkey: string;
-  userSWA: string;
-}
-
-function serializeJSON(obj: any, space: any) {
-  return JSON.stringify(
-    obj,
-    (key, value) =>
-      typeof value === "bigint" ? value.toString() + "n" : value,
-    space
-  );
-}
-
-async function signUserOp(userop: any, sessionConfig: sessionConfig) {
-  const privateKey = sessionConfig.sessionPrivKey as `0x${string}`;
-  const packeduserop = generatePackedUserOp(userop);
-  const hash = generateUserOpHash(packeduserop);
-  const sig = await signMessage({
-    message: {
-      raw: fromHex(hash, "bytes")
-    },
-    privateKey
-  });
-  userop.signature = sig;
-  return userop;
-}
-
-async function executeUserOp(userop: any) {
-  try {
-    const requestBody = {
-      method: "execute",
-      jsonrpc: "2.0",
-      id: uuidv4(),
-      params: [userop],
-    };
-
-    const serliazedPayload = serializeJSON(requestBody, null);
-    const response = await axios.post(
-      "https://sandbox-okto-gateway.oktostage.com/rpc",
-      serliazedPayload,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${OktoAuthToken}`
-        },
-      }
-    );
-
-    return response.data.result.jobId;
-  } catch (error) {
-    console.error("Error executing user operation:", error);
-    throw error;
-  }
-}
-
-async function rawTransaction(data: Data, sessionConfig: sessionConfig) {
+/**
+* Raw Transaction Intent: this function executes raw EVM transactions.
+* For more information, check https://docs.okto.tech/docs/openapi/evmRawTransaction
+*/
+async function rawTransaction(data: Data, sessionConfig: SessionConfig) {
   console.log("Data: ", data);
   console.log("Session Config: ", sessionConfig);
 
@@ -331,7 +46,7 @@ async function rawTransaction(data: Data, sessionConfig: sessionConfig) {
   const jobParametersAbiType = "(string caip2Id, bytes[] transactions)";
   const gsnDataAbiType = `(bool isRequired, string[] requiredNetworks, ${jobParametersAbiType}[] tokens)`;
 
-  //get supported networks call
+  // Fetch all enables networks
   const getChainsResponse = await axios.get("https://sandbox-api.okto.tech/api/oc/v1/supported/networks", {
     headers: {
       "Authorization": `Bearer ${OktoAuthToken}`
@@ -501,7 +216,7 @@ async function rawTransaction(data: Data, sessionConfig: sessionConfig) {
   //   signature: '0x1029e4921213c6f786f4ec9bc3c46bcefc33d7d865165c3f08aafe698da8082d3130bcbdaac4795d87934bfe93e0ac48011e87b985b721bebc7720f8ece7e9d01b'
   // }
 
-  const jobId = await executeUserOp(signedUserOp);
+  const jobId = await executeUserOp(signedUserOp, OktoAuthToken);
   console.log("JobId: ", jobId);
   // Sample Response:
   // JobId: 3ee33731-9e96-4ab9-892c-ea476b36295d
@@ -509,15 +224,16 @@ async function rawTransaction(data: Data, sessionConfig: sessionConfig) {
 
 // To get the caip2Id, please check: https://docsv2.okto.tech/docs/openapi/technical-reference
 const data: Data = {
-  caip2Id: "eip155:84532", // Replace with the actual CAIP-2 ID (e.g., "eip155:1" for Ethereum Mainnet)
+  caip2Id: "eip155:84532", // BASE_TESTNET
   transaction: {
     from: "0x2C6Ef84acD95dA1407712f9Ae4698973D644408b",
     to: "0x967b26c9e77f2f5e0753bcbcb2bb624e5bbff24c",
     data: "0x", // Default empty data
-    value: 1000000000000, // Default value as 0 ETH
+    value: 1000000000000, 
   },
 };
-const sessionConfig: sessionConfig = {
+
+const sessionConfig: SessionConfig = {
   sessionPrivKey:
     "0x096644bf3e32614bb33961d9762d9f2b2768b4ed2e968de2b59c8148875dcec0",
   sessionPubkey:
