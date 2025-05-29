5bc523cd5295a5b1447de6be30e323317b437b07
shouryam-kumar <shouryam1508@gmail.com>
2025-05-29 09:38:05 +0530

    added missing explorer template scripts- getAccount, getTokens, getPortfolioActivity

---
+++ src/api-template/explorer/getAccount.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+const OktoAuthToken = process.env.OKTO_AUTH_TOKEN || "";
+
+/**
+ * Retrieves information about the connected account
+ * 
+ * This function makes an API call to Okto's sandbox API to fetch information about the connected account.
+ * 
+ * @param OktoAuthToken - Authentication token
+ * @returns Object containing details of the connected account
+ *          to the client application. 
+ * 
+ * @throws Error if the API request fails.
+ */
+export async function getAccount(OktoAuthToken: string) {
+    try {
+        const response = await axios.get(
+            "https://sandbox-api.okto.tech/api/oc/v1/wallets",
+            {
+                headers: {
+                    Authorization: `Bearer ${OktoAuthToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching account:", error);
+        throw new Error("Failed to fetch account");
+    }
+}
+
+// Sample usage 
+// const account = await getAccount(OktoAuthToken);
